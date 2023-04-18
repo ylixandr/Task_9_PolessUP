3f9c57e630ce5c0e1716b3add166fa65a10c1e35
ilya <User@WIN-ISS673VO7OO>
2023-04-18 11:00:49 +0300

    Добавьте файлы проекта.

---
+++ Task_6_PolessUP/Program.cs
@@ -0,0 +1,21 @@
+﻿//Реализуйте метод, который определяет для данного года, является ли он високосным.
+//Ввод: 2000
+//Вывод: true
+//Ввод: 2015
+//Вывод: false
+
+static void highAxisYear ()
+{
+
+    bool result = false;
+    DateTime dateTime = DateTime.Now;
+    int year = dateTime.Year;
+    Console.WriteLine(year);
+    if (year % 4 == 0)
+    {
+        result = true;
+    }
+    Console.WriteLine(result);
+}
+
+highAxisYear ();
